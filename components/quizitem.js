2484a5610b615273d80c7e4a66fdefd04edaabf4
witusj <wfth69@gmail.com>
2021-08-07 22:24:27 +0200

    Refactored

--- components/quizitem.js
+++ components/quizitem.js
@@ -1,54 +1,24 @@
 import { Form, Button } from 'react-bootstrap'
 import React, { useState } from 'react'
 
-class quizItem { // The quiz item object
-    constructor(questiontext, trueanswer, falseanswers) {
-        this.questiontext = questiontext
-        this.trueanswer = trueanswer
-        this.falseanswers = falseanswers
-        this.falselength = this.falseanswers.length
-        this.randomnr = (this.falselength + 1) * Math.random()
-        this.trueposition = Math.floor(this.randomnr)
-    }
-
-    shuffleArray() { // This method creates a shuffled array of answer options. The position of the true answer is stored in the variable 'trueposition'
-        const array = this.falseanswers
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            const temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-        }
-        array.splice(this.trueposition, 0, this.trueanswer)
-        return {
-            array
-        }
-    }
-}
-
-const calculateScore = (choice, correct) => {
+const calculateScore = (choice, correct) => { // This function determines the score 
     const score = (choice == correct ? 1 : 0)
     return score
-    console.log(score)
 }
 
-const newItem = new quizItem("To be or not to be?", "YES", ["no", "maybe", "sure", "ok"])
-const truePosition = newItem.trueposition
-const questiontext = newItem.questiontext
-const answerOptions = newItem.shuffleArray()
-
-const QuizItem = () => {
+const QuizItem = (props) => {
     const [choice, setChoice] = useState(-1)
     const [score, setScore] = useState(0)
+    const options = props.options
 
     return (
-        <div>
-            <div className="lead">{questiontext}</div>
-            <div>{truePosition}</div>
+        <div key={props.qid} className="test">
+            <div className="lead">{props.text}</div>
+            <div>{props.trueposition}</div>
             <Form>
-                {answerOptions.array.map((option, index) => ( // Create the answer options and store their array index in the id of the input element
-                    <div key={index} className="form-check">
-                        <input className="input form-check-input" type="radio" name="question1" id={index} onChange={(e) => setChoice(e.target.id)} // If the radio button is checked the button's id is stored in the variable 'choice'
+                {options.map((option, index) => ( // Create the answer options and store their array index in the id of the input element
+                    <div key={index.toString()} className="form-check">
+                        <input className="input form-check-input" type="radio" name={`question${props.qid}`} id={index} onChange={(e) => setChoice(e.target.id)} // If the radio button is checked the button's id is stored in the variable 'choice'
                         />
                         <label className="form-check-label" htmlFor={index}>
                             {option}
@@ -57,8 +27,8 @@ const QuizItem = () => {
                 ))}
                 <Button type="button" className="btn btn-success"
                     onClick={(event) => {
-                        setScore(calculateScore(choice, truePosition))
-                        event.preventDefault()
+                        setScore(calculateScore(choice, props.trueposition))
+                        event.preventDefault() // Important! Otherwise the score will be calculated when a radio button is checked
                     }
                     }>Submit</Button>
             </Form>
