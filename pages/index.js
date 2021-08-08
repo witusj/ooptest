8213e5be1c19f4b7795d086d99e4559336b401bd
witusj <wfth69@gmail.com>
2021-08-08 17:23:23 +0200

    Changed text button Eindscore

--- pages/index.js
+++ pages/index.js
@@ -59,7 +59,7 @@ export default function Home() {
           setTotalscore(sumArray(scoreboard))
           event.preventDefault() // Important! Otherwise the score will be calculated when a radio button is checked
         }
-        }>Submit</Button>
+        }>Eindscore</Button>
       <div>{totalscore}</div>
       <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossOrigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossOrigin="anonymous"></script>
